0b083993401dce3f306501289171fb15d9e08355
way2rach <way2rachita@gmail.com>
2022-02-18 17:03:01 -0500

    add responsive styling for title

--- src/components/sections/RoadMap.js
+++ src/components/sections/RoadMap.js
@@ -59,12 +59,15 @@ const RoadMap = ({
                 <div className={innerClasses}>
                     {/*roadmap section*/}
                     <div className="hero-content">
-                        { !isMobile ? <h1 className="mt-0 mb-16 reveal-from-bottom" data-reveal-delay="200" style={{fontSize: "90px", paddingBottom: "50px"}}>
+                        <h1 className="mt-0 mb-16 reveal-from-bottom roadmap-title" data-reveal-delay="200">
                             ROADMAP
-                        </h1>:
-                        <h1 className="mt-0 mb-16 reveal-from-bottom" data-reveal-delay="200" style={{fontSize: "50px", paddingBottom: "50px"}}>
-                            ROADMAP
-                        </h1>}
+                        </h1>
+                        {/*{ !isMobile ? <h1 className="mt-0 mb-16 reveal-from-bottom" data-reveal-delay="200" style={{fontSize: "90px", paddingBottom: "50px"}}>*/}
+                        {/*    ROADMAP*/}
+                        {/*</h1>:*/}
+                        {/*<h1 className="mt-0 mb-16 reveal-from-bottom" data-reveal-delay="200" style={{fontSize: "50px", paddingBottom: "50px"}}>*/}
+                        {/*    ROADMAP*/}
+                        {/*</h1>}*/}
                     </div>
                     <hr/>
                     {/*claiming section*/}
