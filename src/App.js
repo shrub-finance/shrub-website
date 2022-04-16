16a28449a925946cc395da6bea0537c495a9a867
way2rach <way2rachita@gmail.com>
2022-04-15 22:37:44 -0400

    update roadmap link

--- src/App.js
+++ src/App.js
@@ -56,8 +56,8 @@ const App = () => {
         <Switch>
           <AppRoute exact path="/" component={Main} layout={LayoutDefault} />
           <AppRoute exact path="/main" component={Home} layout={LayoutDefault} />
-          <AppRoute exact path="/roadmap" component={Main} layout={LayoutDefault} />
-          <AppRoute exact path="/airdrop" component={AirDrop} layout={LayoutDefaultSlim} />
+          <AppRoute exact path="/roadmap" component={Home} layout={LayoutDefault} />
+          <AppRoute exact path="/airdrop" component={Home} layout={LayoutDefaultSlim} />
           <AppRoute exact path="/paper-gardens" component={Chapters} layout={LayoutDefaultSlim} />
           <AppRoute exact path="/about" component={About} layout={LayoutDefault} />
         </Switch>
