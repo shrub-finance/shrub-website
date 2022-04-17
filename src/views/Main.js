8368c9bf4bbe188714ab3e5689c10e701cd5c364
way2rach <way2rachita@gmail.com>
2022-04-17 00:19:21 -0400

    add banner

--- src/views/Main.js
+++ src/views/Main.js
@@ -7,12 +7,15 @@ import Team from "../components/sections/Team";
 import Grants from "../components/sections/Grants";
 import ShrubPaper from "../components/sections/ShrubPaper";
 import PaperGardens from "../components/sections/PaperGardens";
+import ReactGA from "react-ga";
 
 
 const Main = () => {
 
     return (
-        <>
+        <><div className={"banner"}>&#129489;&#8205;&#127806; <ReactGA.OutboundLink eventLabel="Banner link to Paper gardens" to="https://gardens.shrub.finance" target="_blank" rel='noopener noreferrer'>
+           &nbsp; <a title="Go to Paper Gardens App">Shrub's Genesis NFT series - Paper Gardens </a>
+        </ReactGA.OutboundLink></div>
             <Hero className="illustration-section-01" />
             <ShrubStory invertMobile imageFill className="illustration-section-02" />
             <ShrubPaper/>
