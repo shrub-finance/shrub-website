50521879e950ca668891b6e47254a169038ecae9
way2rach <way2rachita@gmail.com>
2022-05-03 13:28:04 -0400

    update banner

--- src/views/Main.js
+++ src/views/Main.js
@@ -8,13 +8,16 @@ import Grants from "../components/sections/Grants";
 import ShrubPaper from "../components/sections/ShrubPaper";
 import PaperGardens from "../components/sections/PaperGardens";
 import ReactGA from "react-ga";
+import {isMobile} from "react-device-detect";
 
 
 const Main = () => {
 
     return (
-        <><div className={"banner"}><ReactGA.OutboundLink eventLabel="Banner link to Paper gardens" to="https://gardens.shrub.finance" target="_blank" rel='noopener noreferrer'><a title="Go to Paper Gardens App">Shrub's Genesis NFT series - Paper Gardens </a>
-        </ReactGA.OutboundLink></div>
+        <>{!isMobile ?<div className="banner">Shrub's Genesis NFT series is here!&nbsp;&nbsp; <ReactGA.OutboundLink eventLabel="Banner link to Paper gardens" to="https://gardens.shrub.finance" target="_blank" rel='noopener noreferrer'><a title="Go to Paper Gardens App">Checkout Paper Gardens</a>
+        </ReactGA.OutboundLink></div>: <div className="banner"><ReactGA.OutboundLink eventLabel="Banner link to Paper gardens" to="https://gardens.shrub.finance" target="_blank" rel='noopener noreferrer'><a title="Go to Paper Gardens App">Checkout Shrub's Genesis NFT series Paper Gardens</a>
+        </ReactGA.OutboundLink></div>}
+
             <Hero className="illustration-section-01" />
             <ShrubStory invertMobile imageFill className="illustration-section-02" />
             <ShrubPaper/>
