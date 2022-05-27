aa1bb941de445d8e10c07376a66098e367a8953d
way2rach <way2rachita@gmail.com>
2022-05-27 13:26:23 -0400

    update banner

--- src/views/Main.js
+++ src/views/Main.js
@@ -16,8 +16,9 @@ import Tickets from "../components/sections/Tickets";
 const Main = () => {
 
     return (
-        <>{!isMobile ?<div className="banner">❤️ Participate in Shrub's Genesis NFT series &nbsp;<ReactGA.OutboundLink eventLabel="Banner link to Paper gardens" to="https://gardens.shrub.finance" target="_blank" rel='noopener noreferrer'><a title="Go to Paper Gardens App">Paper Gardens</a>
-        </ReactGA.OutboundLink></div>: <div className="banner"><ReactGA.OutboundLink eventLabel="Banner link to Paper gardens" to="https://gardens.shrub.finance" target="_blank" rel='noopener noreferrer'><a title="Go to Paper Gardens App">❤️ Participate in Shrub's Genesis NFT series</a>
+        <>{!isMobile ?<div className="banner">🎉 Paper Gardens NFT Sale Dates Announced!
+            &nbsp;<ReactGA.OutboundLink eventLabel="Banner link to Paper gardens" to="https://medium.com/@shrubfinance/paper-gardens-sale-date-announcement-265ceaa9e21e" target="_blank" rel='noopener noreferrer'><a title="Go to Paper Gardens App">Learn More</a>
+        </ReactGA.OutboundLink></div>: <div className="banner"><ReactGA.OutboundLink eventLabel="Banner link to Paper gardens" to="https://medium.com/@shrubfinance/paper-gardens-sale-date-announcement-265ceaa9e21e" target="_blank" rel='noopener noreferrer'><a title="Go to Paper Gardens App">🎉 Paper Gardens NFT Sale Dates Announced!</a>
         </ReactGA.OutboundLink></div>}
 
             <Hero className="illustration-section-01" />
